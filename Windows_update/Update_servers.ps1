a204cfd719b4c6c202528195ff8da55d5adffc47
Alpha725 <48570690+Alpha725@users.noreply.github.com>
2021-04-13 15:07:15 +1000

    Update Update_servers.ps1
    updates after learning the limitations of remote command execution and realising that

--- Windows_update/Update_servers.ps1
+++ Windows_update/Update_servers.ps1
@@ -1,8 +1,15 @@
-$script = Get-Content .\Windows_update.ps1
-$manual_servers = Get-Content .\manual_patching.txt
-foreach ($server in $manual_servers) {
-	start-process Invoke-Command -ComputerName $server -Command {
-	$script >> c:\temp\Windows_update.ps1
-	c:\temp\Windows_update.ps1 YES YES
-	}
+## script is to more place a script onto a server and run it
+## it is "Multithreaded" as it will not wait for servers to complete before moving on.
+## you will need to run ./wmiobject_fix.ps1 before this works
+## Note you can pull the server name as you wish, just change out the $servers variable
+
+$servers = Get-WsusComputer -ComputerTargetGroups 'Manual Patching'  | Where-Object {$_.FullDomainName -cnotlike "*wsus*"}
+
+foreach ($server in $servers) {
+    $computer = $server.FullDomainName
+    write-host starting update on $computer
+    copy c:\Windows\System32\Windows_update.ps1 \\$computer\c$\temp\
+    Invoke-Command -ComputerName $computer -ConfigurationName 'VirtualAccount' -Command {
+        Start-Process -FilePath "c:\temp\Windows_update.ps1" -ArgumentList "YES YES"
+    }
 }
